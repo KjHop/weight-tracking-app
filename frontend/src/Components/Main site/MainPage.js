3f075e8a19612ef9d09e75b22264ade064651e7d
kjhop <kjhop@wp.pl>
2019-02-12 20:32:39 +0100

    Changed main page to more minimalist version

--- frontend/src/Components/Main site/MainPage.js
+++ frontend/src/Components/Main site/MainPage.js
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import "../../Styles/MainPage.css";
-import NavigationBar from "./NavigationBar";
+import {Link} from 'react-router-dom';
 
 class MainPage extends React.Component{
     render(){
         return(
             <div className="MainPage">
-                <NavigationBar/>
+                <div className="MainPageHeading">
+                    <h1>Light weight</h1>
+                    <h2>More than just a weight tracking app</h2>
+                    <button className="MainPageSignup">Sign Up</button>
+                    <h3>Already a member? <span className="MainPageSignin"><Link to="/login">Sign In</Link></span></h3>
+                </div>
             </div>
         )
     }
--- frontend/src/Components/Main site/NavigationBar.js
+++ frontend/src/Components/Main site/NavigationBar.js
@@ -6,9 +6,17 @@ class NavigationBar extends React.Component{
     render(){
         return(
             <div className="NavigationBar">
-                <Link to="/about">About</Link>
-                <Link to="/graph">Graph</Link>
-                <Link to="/journal">Journal</Link>
+                <ul>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/graph">Graph</Link>
+                    </li>
+                    <li>
+                        <Link to="/journal">Journal</Link>
+                    </li>
+                </ul>
             </div>
         )
     }
