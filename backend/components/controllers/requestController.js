4e3b1dcea3e1de92781ed08c4c9f28fdb2c37775
kjhop <kjhop@wp.pl>
2019-02-14 19:41:12 +0100

    Simple registration and login system

--- backend/components/controllers/requestController.js
+++ backend/components/controllers/requestController.js
@@ -3,7 +3,16 @@ const mysql = require("mysql");
 
 module.exports = app =>{
     app.get("/",(request,response)=>{
-        response.send("Gitara");
+        const pool = mysql.createPool({
+            connectionLimit: 10,
+            host:"localhost",
+            user:"test",
+            password:"testtest1",
+            database:'light_weight'
+        });
+        pool.query("SELECT * FROM users", (error, result)=>{
+            console.log(result);
+        });
     });
     app.post("/login", (request,response)=>{
         const pool = mysql.createPool({
@@ -13,10 +22,20 @@ module.exports = app =>{
             password:"testtest1",
             database:'light_weight'
         });
-        const sql = "SELECT username, password FROM users";
+        const sql = "SELECT username, password FROM users WHERE username='"+request.body.username+"' and password='"+request.body.password+"'";
         pool.query(sql, (error, result)=>{
             if (error) throw error;
-            response.send(result);
+            console.log(result[0]);
+            console.log(result[0].username==request.body.username, result[0].username, request.body.username);
+            if(result.length!==0 && result !== undefined){
+                if(result[0].username===request.body.username && result[0].password===request.body.password){
+                    response.send("Logged in");
+                }else{
+                    response.send("Wrong username or password");
+                }
+            }else{
+                response.send("Bad username or password");
+            }
         });
     });
     app.post("/register", (request, response)=>{
@@ -28,10 +47,17 @@ module.exports = app =>{
             database:'light_weight'
         });
         const sql = "INSERT INTO users(username, password, email) VALUES('"+request.body.username+"','"+request.body.password+"','"+request.body.email+"')";
-        pool.query(sql, (error, result)=>{
+        //Check if user exists
+        pool.query("SELECT username FROM users WHERE username='"+request.body.username+"'", (error, result)=>{
             if (error) throw error;
-            console.log("Successfully registered");
-            response.send("Gud gud");
+            if(result.length===0 && result !== undefined){
+                pool.query(sql, (error, result)=>{
+                    console.log("Successfully registered");
+                    response.send("Gud gud");
+                });
+            }else{
+                response.send("User exists");
+            }
         });
     });
 }
